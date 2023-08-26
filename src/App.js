55a8608106690dab80cbd4d18506464cbec32902
devdivyansh522399 <divyanshdev87070@gmail.com>
2023-08-27 02:19:07 +0530

    MERN Beginner application

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PageNotFound from "./pages/PageNotFound";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
+import ForgotPassword from "./pages/Auth/ForgotPassword";
+import AdminRoute from "./components/Routes/AdminRoutes";
+import AdminDashBoard from "./pages/Admin/AdminDashBoard";
+import CreateProduct from "./pages/Admin/CreateProduct";
+import CreateCategory from "./pages/Admin/CreateCategory";
+import Users from "./pages/Admin/Users";
+import Product from "./pages/Admin/Product";
+import UpdateProduct from "./pages/Admin/UpdateProduct";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="user" element={<Dashboard />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashBoard />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/product" element={<Product />} />
+          <Route path="admin/users" element={<Users />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/images.js
@@ -0,0 +1,7 @@
+import landingPage from './assests/loginPage.jpg'
+
+const images = {
+    landingPage
+}
+
+export default images
